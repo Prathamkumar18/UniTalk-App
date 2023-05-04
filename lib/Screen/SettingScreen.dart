093c7aeb38a39bb3636274e5c61de6983d0415ec
Pratham Kumar <pratham854305@gmail.com>
2023-05-04 21:53:16 +0530

    Settings UI

--- lib/Screen/SettingScreen.dart
+++ lib/Screen/SettingScreen.dart
@@ -1,10 +1,222 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:ffi';
 
-class SettingScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:uni_talk/Utils/Colors.dart';
+import 'package:uni_talk/resources/firestore_methods.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../resources/auth_method.dart';
+
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  bool onTapNameEdit = false;
+  bool onTapPasswordEdit = false;
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+  FirestoreMethods _firestoreMethods = FirestoreMethods();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: tintWhite,
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30.0, left: 16.0, right: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Hello",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
+                TextIcon("Pratham", Icon(Icons.edit), 30),
+                SizedBox(
+                  height: 10,
+                ),
+                if (onTapNameEdit)
+                  EditBox("Username", Icon(Icons.person), username),
+                TextIcon("Change Password ?", Icon(Icons.edit), 30),
+                SizedBox(
+                  height: 10,
+                ),
+                if (onTapPasswordEdit)
+                  EditBox("Password", Icon(Icons.password), password),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("Deactivate Account:",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
+                Text(
+                    "Once you deactivate your account, there is no going back. Please be certain.",
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Color.fromARGB(255, 73, 206, 251)),
+                        color: Color.fromARGB(255, 8, 0, 49),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "DeActivate",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 227, 111, 103),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget TextIcon(String text, Icon icon, double fontS) {
+    return Row(
+      children: [
+        Text(text,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: fontS,
+                fontWeight: FontWeight.bold)),
+        SizedBox(
+          width: 20,
+        ),
+        InkWell(
+            onTap: () {
+              setState(() {
+                if (text == "Change Password ?") {
+                  onTapPasswordEdit = !onTapPasswordEdit;
+                } else
+                  onTapNameEdit = !onTapNameEdit;
+              });
+            },
+            child: icon)
+      ],
+    );
+  }
+
+  Widget EditBox(String text, Icon icon, TextEditingController controller) {
+    return Column(
+      children: [
+        TextFormField(
+          keyboardType: TextInputType.name,
+          controller: controller,
+          validator: (value) {
+            if (value!.isEmpty) {
+              return "Enter $text";
+            } else {
+              return null;
+            }
+          },
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              hintText: text,
+              label: Text(
+                text,
+                style: TextStyle(fontWeight: FontWeight.w400),
+              ),
+              prefixIcon: icon),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 40,
+              width: 140,
+              child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(black)),
+                  onPressed: () {},
+                  child: Text("save",
+                      style: TextStyle(fontSize: 20, color: Colors.white))),
+            ),
+            Container(
+              height: 40,
+              width: 140,
+              child: TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(black)),
+                  onPressed: () {
+                    setState(() {
+                      if (text == "Username") {
+                        onTapNameEdit = false;
+                      } else
+                        onTapPasswordEdit = false;
+                    });
+                  },
+                  child: Text("cancel",
+                      style: TextStyle(fontSize: 20, color: Colors.white))),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        if (text == "Password")
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Want to generate Password?",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              InkWell(
+                onTap: () async {
+                  final url =
+                      'https://priyanshu25022002raj.github.io/pass.github.io/';
+                  await launch(url,
+                      forceSafariVC: true,
+                      forceWebView: true,
+                      enableJavaScript: true);
+                },
+                child: Text(
+                  "click here",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800),
+                ),
+              ),
+            ],
+          ),
+        SizedBox(
+          height: 10,
+        )
+      ],
+    );
   }
 }
--- lib/Screen/historyMeetingScreen.dart
+++ lib/Screen/historyMeetingScreen.dart
@@ -1,45 +1,146 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:uni_talk/resources/firestore_methods.dart';
 
-class HistoryMeetingScreen extends StatelessWidget {
+class HistoryMeetingScreen extends StatefulWidget {
   const HistoryMeetingScreen({super.key});
 
   @override
+  State<HistoryMeetingScreen> createState() => _HistoryMeetingScreenState();
+}
+
+class _HistoryMeetingScreenState extends State<HistoryMeetingScreen> {
+  bool _isDelayed = true;
+  var timer;
+
+  @override
+  void initState() {
+    _isDelayed = true;
+    super.initState();
+    timer = Timer(Duration(milliseconds: 1500), () {
+      setState(() {
+        _isDelayed = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    timer;
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: FirestoreMethods().meetingsHistory,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
+    return (_isDelayed)
+        ? Shimmer.fromColors(
+            baseColor: Color.fromARGB(255, 0, 17, 255),
+            highlightColor: Color.fromARGB(255, 0, 242, 255),
             child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: (snapshot.data! as dynamic).docs.length,
-                itemBuilder: (context, index) => Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(50))),
-                        tileColor: Color.fromARGB(255, 229, 243, 250),
-                        leading: Icon(
-                          Icons.history,
-                          size: 30,
-                          color: Colors.black,
-                        ),
-                        title: Text(
-                          "Room No: ${(snapshot.data! as dynamic).docs[index]['roomName']}",
-                          style: TextStyle(fontSize: 22),
-                        ),
-                        subtitle: Text(
-                            "Joined on: ${DateFormat.yMMMd().format((snapshot.data! as dynamic).docs[index]['createdAt'].toDate())}",
-                            style: TextStyle(fontSize: 16)),
+              shrinkWrap: true,
+              itemCount: 8,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      dummyShimmer(60, 60),
+                      SizedBox(
+                        width: 20,
                       ),
-                    )),
-          );
-        });
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          dummyShimmer(20, 260),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          dummyShimmer(20, 230),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        : StreamBuilder(
+            stream: FirestoreMethods().meetingsHistory,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Shimmer.fromColors(
+                  baseColor: Color.fromARGB(255, 0, 17, 255),
+                  highlightColor: Color.fromARGB(255, 0, 242, 255),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            dummyShimmer(60, 60),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                dummyShimmer(20, 260),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                dummyShimmer(20, 230),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(50))),
+                            tileColor: Color.fromARGB(255, 229, 243, 250),
+                            leading: Icon(
+                              Icons.history,
+                              size: 30,
+                              color: Colors.black,
+                            ),
+                            title: Text(
+                              "Room No: ${(snapshot.data! as dynamic).docs[index]['roomName']}",
+                              style: TextStyle(fontSize: 22),
+                            ),
+                            subtitle: Text(
+                                "Joined on: ${DateFormat.yMMMd().format((snapshot.data! as dynamic).docs[index]['createdAt'].toDate())}",
+                                style: TextStyle(fontSize: 16)),
+                          ),
+                        )),
+              );
+            });
+  }
+
+  Widget dummyShimmer(double h, double w) {
+    return Container(
+      height: h,
+      width: w,
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.04),
+          borderRadius: BorderRadius.circular(10)),
+    );
   }
 }
